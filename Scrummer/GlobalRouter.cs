d202198ff1b2ceefb4cdbf95239ff98b126318bb
Valeriano A.R <valen.alfonso@gmail.com>
2015-05-23 18:51:22 +0200

    First commit

---
+++ Scrummer/GlobalRouter.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Scrummer
+{
+    public class GlobalRouter : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("Hello world!");
+        }
+    }
+}
