0517f70f4473397a9daf67e1e76c17668c6bdee5
Valeriano A.R <valen.alfonso@gmail.com>
2015-05-24 04:47:27 +0200

    CLabel: Custom label with tag parameter.

---
+++ Scrummer/Code/Controls/CLabel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Scrummer.Code.Controls
+{
+    public class CLabel : Label
+    {
+        #region Declarations
+
+        private string _tagName = "span";
+
+        #endregion
+
+        #region Properties
+
+        public string Tag
+        {
+            get { return _tagName; }
+            set { _tagName = value; }
+        }
+
+        #endregion
+
+        #region Life cycle
+
+        public override void RenderBeginTag(HtmlTextWriter writer)
+        {
+            if (string.IsNullOrEmpty(_tagName) == false)
+            {
+                this.AddAttributesToRender(writer);
+                writer.RenderBeginTag(_tagName);
+            }
+            else
+            {
+                base.RenderBeginTag(writer);
+            }
+        }
+
+        #endregion
+    }
+}
