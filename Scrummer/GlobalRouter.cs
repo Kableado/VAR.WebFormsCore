b35379b8fed204019dc059ec2d84f0dd7475e1ab
Valeriano A.R <valen.alfonso@gmail.com>
2015-05-24 15:55:57 +0200

    GlobalRouter: Route request to types.

--- Scrummer/GlobalRouter.cs
+++ Scrummer/GlobalRouter.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Web;
 using Scrummer.Code;
 using Scrummer.Code.JSON;
@@ -7,6 +10,77 @@ namespace Scrummer
 {
     public class GlobalRouter : IHttpHandler
     {
+        #region Handlers
+
+        private static Dictionary<string, Type> _handlers = new Dictionary<string, Type>();
+
+        private static IHttpHandler GetHandler(string typeName)
+        {
+            if (string.IsNullOrEmpty(typeName)) { return null; }
+            Type type = null;
+            if (_handlers.ContainsKey(typeName))
+            {
+                type = _handlers[typeName];
+                IHttpHandler handler = Activator.CreateInstance(type) as IHttpHandler;
+                return handler;
+            }
+
+            // Search type on executing assembly
+            Type[] types;
+            Assembly asm = Assembly.GetExecutingAssembly();
+            types = asm.GetTypes();
+            foreach (Type typeAux in types)
+            {
+                if (typeAux.FullName.EndsWith(typeName))
+                {
+                    type = typeAux;
+                    break;
+                }
+            }
+
+            // Search type on all loaded assemblies
+            if (type == null)
+            {
+                Assembly[] asms = AppDomain.CurrentDomain.GetAssemblies();
+                foreach (Assembly asmAux in asms)
+                {
+                    types = asmAux.GetTypes();
+                    foreach (Type typeAux in types)
+                    {
+                        if (typeAux.FullName.EndsWith(typeName))
+                        {
+                            type = typeAux;
+                            break;
+                        }
+                    }
+                    if (type != null) { break; }
+                }
+            }
+
+            // Use found type
+            if (type != null)
+            {
+                IHttpHandler handler = Activator.CreateInstance(type) as IHttpHandler;
+                if (handler != null)
+                {
+                    lock (_handlers)
+                    {
+                        if (_handlers.ContainsKey(typeName) == false)
+                        {
+                            _handlers.Add(typeName, type);
+                        }
+                    }
+                }
+                return handler;
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region IHttpHandler
+
         public bool IsReusable
         {
             get { return false; }
@@ -16,12 +90,29 @@ namespace Scrummer
         {
             try
             {
-                context.Response.Write("Hello world!");
+                string file = Path.GetFileName(context.Request.FilePath);
+                if (string.IsNullOrEmpty(file))
+                {
+                    // TODO: Default Handler
+                }
+                IHttpHandler handler = GetHandler(file);
+                if (handler == null)
+                {
+                    // TODO: FrmNotFound
+                    throw new Exception("NotFound");
+                }
+
+                // Use handler
+                context.Response.Clear();
+                context.Handler = handler;
+                handler.ProcessRequest(context);
             }
             catch (Exception ex)
             {
                 GlobalErrorHandler.HandleError(context, ex);
             }
         }
+
+        #endregion
     }
 }
