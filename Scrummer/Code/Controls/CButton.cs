3570859bf41d7636758ffc1d7ad32cddda42d5ec
Valeriano A.R <valen.alfonso@gmail.com>
2015-06-03 00:36:31 +0200

    CButton

---
+++ Scrummer/Code/Controls/CButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Scrummer.Code.Controls
+{
+    public class CButton : Button
+    {
+        public CButton()
+        {
+            CssClass = "button";
+        }
+    }
+}
