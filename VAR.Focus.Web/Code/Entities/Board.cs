987e23e48747e3324ea6f6ed261637d24d15907b
Valeriano A.R <valen.alfonso@gmail.com>
2015-11-09 07:16:25 +0100

    Entities.Board: Active by default on new instances.

--- VAR.Focus.Web/Code/Entities/Board.cs
+++ VAR.Focus.Web/Code/Entities/Board.cs
@@ -9,7 +9,8 @@ namespace VAR.Focus.Web.Code.Entities
         public string Title { get; set; }
         public string Description { get; set; }
 
-        public bool Active { get; set; }
+        private bool _active = true;
+        public bool Active { get { return _active} set { _active = value; } }
         public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string ModifiedBy { get; set; }
